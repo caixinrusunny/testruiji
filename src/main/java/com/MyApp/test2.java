9e8f82fcb97193bfa58d570ee6fd87b829eb7efb
菜心 <2217298571@qq.com>
2022-12-15 16:29:39 +0800

    test2测试测回的提交

--- src/main/java/com/MyApp/test2.java
+++ src/main/java/com/MyApp/test2.java
@@ -1,7 +1,7 @@
 package com.MyApp;
 
-public class test1 {
+public class test2 {
     public static void main(String[] args) {
-        System.out.println("xixix");
+        System.out.println("lalal");
     }
 }
